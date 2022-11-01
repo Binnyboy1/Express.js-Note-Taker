8a177626bceaf2e5c1efc73d86414710a3cf4866
Binny <ibinstock@outlook.com>
2022-10-31 21:35:56 -0800

    banged out GET routes without much hassle

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -3,18 +3,26 @@ const express = require('express');
 const path = require('path');
 
 // TODO: Import 'db.json' file
-const terms = require('../../../db/db.json');
+const database = require('../../../db/db.json');
 
 const PORT = 3001;
 const app = express();
 
-// TODO: Initialize app variable
-app.get('/', (req, res) => res.send('Test /terms'));
-
-// TODO: Create a route for a GET request that will return the content of our `terms.json` file
-app.get('/terms', (req, res) =>
-  res.json(terms)
+// GET Route for homepage ✅-Working
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../../index.html'))
 );
+
+// GET Route for notes page ✅-Working
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../../notes.html'))
+);
+
+// GET Route that will return the content of our `db.json` file ✅-Working
+app.get('/api/notes', (req, res) =>
+  res.json(database)
+);
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
